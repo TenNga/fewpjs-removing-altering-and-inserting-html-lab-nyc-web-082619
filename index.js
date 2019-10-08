5e1b583225e6a2bb7c07b669eafdf51beeebe4c7
Kevin Karma <kevinkarma55@gmail.com>
2019-10-08 16:48:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
 document.querySelector('#main').remove();
-let header = document.createElement('h1#victory');
-document.body.appendChild(header);
 let newHeader = document.querySelector('h1#victory');
